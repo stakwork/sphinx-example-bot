17fc5ba7cfe4219ef25bba3823ef431f22889129
Evanfeenstra <evanfeenstra@gmail.com>
2021-08-09 13:28:45 -0700

    example bot

---
+++ README.md
@@ -0,0 +1,15 @@
+# sphinx-example-bot
+
+An example of how to create bots on Sphinx.
+
+### build
+
+`npm install`
+
+`npm run build`
+
+`npm run start`
+
+### create a bot in your Sphinx app
+
+For local dev, set the Bot Webhook to `http://localhost:3000`. Take the Bot Token, and put it in a `.env` file under like `SPHINX_TOKEN=*******`
---
+++ index.js
@@ -0,0 +1,57 @@
+import "regenerator-runtime/runtime.js";
+import * as Sphinx from "sphinx-bot";
+require("dotenv").config();
+const msg_types = Sphinx.MSG_TYPE;
+
+let initted = false;
+
+const sphinxToken = process.env.SPHINX_TOKEN;
+
+function init() {
+  if (initted) return;
+  initted = true;
+
+  const client = new Sphinx.Client();
+  client.login(sphinxToken);
+
+  client.on(msg_types.INSTALL, async (message) => {
+    const embed = new Sphinx.MessageEmbed()
+      .setAuthor("ExampleBot")
+      .setDescription("Welcome to Example Bot!")
+      .setThumbnail(botSVG);
+    message.channel.send({ embed });
+  });
+
+  client.on(msg_types.MESSAGE, async (message) => {
+    const arr = message.content.split(" ");
+    if (arr.length < 2) return;
+    if (arr[0] !== "/example") return;
+    const cmd = arr[1];
+
+    const isAdmin = message.member.roles.find((role) => role.name === "Admin");
+    console.log("isAdmin?", isAdmin);
+
+    switch (cmd) {
+      case "test":
+        const embed = new Sphinx.MessageEmbed()
+          .setAuthor("ExampleBot")
+          .setTitle("A Test:")
+          .addFields([
+            { name: "Hi:", value: "Hello", inline: true },
+            { name: "Hi 2:", value: "Hello 2", inline: true },
+          ]);
+        message.channel.send({ embed });
+        return;
+
+      default:
+        const embed = new Sphinx.MessageEmbed()
+          .setAuthor("ExampleBot")
+          .setTitle("ExampleBot Commands:")
+          .addFields([{ name: "test", value: "/example test" }]);
+        message.channel.send({ embed });
+        return;
+    }
+  });
+}
+
+init();

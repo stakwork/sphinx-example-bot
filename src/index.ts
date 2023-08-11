f75c3ff42ea0bb8a9fec3bdcb1483b015921ce3d
Oluwatobi Bamidele <teebams49@gmail.com>
2023-08-11 14:56:16 +0100

    update: updated content to typescript

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,58 @@
-console.log("Hello world!");
+import * as Sphinx from "sphinx-bot";
+require("dotenv").config();
+const msg_types = Sphinx.MSG_TYPE;
+
+let initted = false;
+
+const sphinxToken = process.env.SPHINX_TOKEN || "";
+
+const PREFIX = "example";
+function init() {
+  if (initted) return;
+  initted = true;
+
+  const client = new Sphinx.Client();
+  client.login(sphinxToken);
+
+  client.on(msg_types.INSTALL, async (message) => {
+    console.log("=> Received an install!");
+    const embed = new Sphinx.MessageEmbed()
+      .setAuthor("ExampleBot")
+      .setDescription("Welcome to Example Bot!");
+    message.channel.send({ embed });
+  });
+
+  client.on(msg_types.MESSAGE, async (message) => {
+    console.log("=> Received a message!");
+    const arr = message.content.split(" ");
+    if (arr.length < 2) return;
+    if (arr[0] !== "/" + PREFIX) return;
+    const cmd = arr[1];
+
+    const isAdmin = message.member.roles.find((role) => role.name === "Admin");
+    console.log("isAdmin?", isAdmin);
+
+    switch (cmd) {
+      case "test":
+        const embed = new Sphinx.MessageEmbed()
+          .setAuthor("ExampleBot")
+          .setTitle("A Test!!!!!")
+          .addFields([
+            { name: "Hi:", value: "Hello", inline: true },
+            { name: "Hi 2:", value: "Hello 2", inline: true },
+          ]);
+        message.channel.send({ embed });
+        return;
+
+      default:
+        const embed2 = new Sphinx.MessageEmbed()
+          .setAuthor("ExampleBot")
+          .setTitle("ExampleBot Commands:")
+          .addFields([{ name: "test", value: "/example test" }]);
+        message.channel.send({ embed: embed2 });
+        return;
+    }
+  });
+}
+
+init();
